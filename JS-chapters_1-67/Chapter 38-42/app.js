d2509ea17f70fbc05557f9707e86fea8446e10fc
Arsalan Sheikh <41226869+ARSSHEIKH@users.noreply.github.com>
2020-07-25 16:43:38 +0500

    Updated files

--- JS-chapters_1-67/Chapter 38-42/app.js
+++ JS-chapters_1-67/Chapter 38-42/app.js
@@ -26,4 +26,48 @@
 // let S = calculate_S(2, 3, 4);
 // document.write("Area of Trianagle: ", area_Trianagle(S, 2, 3, 4));
 
-// Example 4
+// // Example 4
+// function mainFunction(){
+//     var sub1, sub2, sub3;
+//     sub1 = 50, sub2 = 75, sub3 = 87;
+//     document.write("The Percentage is: ", percentage(average(sub1, sub2, sub3)));
+    
+//     function average(sub1_in, sub2_in, sub3_in){
+//         return sub1_in + sub2_in +sub3_in;
+//     }
+//     function percentage(avg){
+//       return ((avg*100)/300).toFixed(2);
+//     }
+// }
+// mainFunction()
+
+// // Example 5
+// function index_of(str, letter){
+//     let arr = str.split("", str.length);
+//     let ind=0;
+//     let found = false
+//     while(ind<arr.length){
+//         if(letter == arr[ind]){
+//             found=true;
+//             return ind;
+//         }
+//         ind++;
+//     }
+//     if(found == false){
+//         return "sorry, you entered an invalid character"
+//     }
+// }
+// var obj1 = {str: prompt("Enter any string: ")};
+// var indof = prompt(`Enter index of any character of ${obj1.str}`);
+// document.write("Index of ", indof, " is :", index_of(obj1.str, indof));
+
+
+
+
+
+
+
+
+
+
+
